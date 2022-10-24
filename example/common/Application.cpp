83b9f1e1131b2c0a03fd9191d71e880690d45fbb
Jake <>
2022-10-24 03:22:19 -0700

    Added a simple Application abstraction and some associated utilities

---
+++ example/common/Application.cpp
@@ -0,0 +1,255 @@
+#include "Application.h"
+
+#include <exception>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+#include <Fwog/DebugMarker.h>
+
+#include <glm/gtc/constants.hpp>
+
+namespace
+{
+  void GLAPIENTRY OpenglErrorCallback(GLenum source,
+                                      GLenum type,
+                                      GLuint id,
+                                      GLenum severity,
+                                      [[maybe_unused]] GLsizei length,
+                                      const GLchar* message,
+                                      [[maybe_unused]] const void* userParam)
+  {
+    // Ignore certain verbose info messages (particularly ones on Nvidia).
+    if (id == 131169 || id == 131185 || id == 131218 || id == 131204 || id == 0)
+      return;
+
+    std::stringstream errStream;
+    errStream << "OpenGL Debug message (" << id << "): " << message << '\n';
+
+    switch (source)
+    {
+    case GL_DEBUG_SOURCE_API: errStream << "Source: API"; break;
+    case GL_DEBUG_SOURCE_WINDOW_SYSTEM: errStream << "Source: Window Manager"; break;
+    case GL_DEBUG_SOURCE_SHADER_COMPILER: errStream << "Source: Shader Compiler"; break;
+    case GL_DEBUG_SOURCE_THIRD_PARTY: errStream << "Source: Third Party"; break;
+    case GL_DEBUG_SOURCE_APPLICATION: errStream << "Source: Application"; break;
+    case GL_DEBUG_SOURCE_OTHER: errStream << "Source: Other"; break;
+    }
+
+    errStream << '\n';
+
+    switch (type)
+    {
+    case GL_DEBUG_TYPE_ERROR: errStream << "Type: Error"; break;
+    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: errStream << "Type: Deprecated Behaviour"; break;
+    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: errStream << "Type: Undefined Behaviour"; break;
+    case GL_DEBUG_TYPE_PORTABILITY: errStream << "Type: Portability"; break;
+    case GL_DEBUG_TYPE_PERFORMANCE: errStream << "Type: Performance"; break;
+    case GL_DEBUG_TYPE_MARKER: errStream << "Type: Marker"; break;
+    case GL_DEBUG_TYPE_PUSH_GROUP: errStream << "Type: Push Group"; break;
+    case GL_DEBUG_TYPE_POP_GROUP: errStream << "Type: Pop Group"; break;
+    case GL_DEBUG_TYPE_OTHER: errStream << "Type: Other"; break;
+    }
+
+    errStream << '\n';
+
+    switch (severity)
+    {
+    case GL_DEBUG_SEVERITY_HIGH: errStream << "Severity: high"; break;
+    case GL_DEBUG_SEVERITY_MEDIUM: errStream << "Severity: medium"; break;
+    case GL_DEBUG_SEVERITY_LOW: errStream << "Severity: low"; break;
+    case GL_DEBUG_SEVERITY_NOTIFICATION: errStream << "Severity: notification"; break;
+    }
+
+    std::cout << errStream.str() << '\n';
+  }
+} // namespace
+
+// This class provides static callbacks for GLFW.
+// It has access to the private members of Application and assumes a pointer to it is present in the window's user pointer.
+class ApplicationAccess
+{
+public:
+  static void CursorPosCallback(GLFWwindow* window, double currentCursorX, double currentCursorY)
+  {
+    Application* app = static_cast<Application*>(glfwGetWindowUserPointer(window));
+    if (app->cursorJustEnteredWindow)
+    {
+      app->previousCursorPos = {currentCursorX, currentCursorY};
+      app->cursorJustEnteredWindow = false;
+    }
+
+    app->cursorFrameOffset +=
+        glm::dvec2{currentCursorX - app->previousCursorPos.x, app->previousCursorPos.y - currentCursorY};
+    app->previousCursorPos = {currentCursorX, currentCursorY};
+  }
+
+  static void CursorEnterCallback(GLFWwindow* window, int entered)
+  {
+    Application* app = static_cast<Application*>(glfwGetWindowUserPointer(window));
+    if (entered)
+    {
+      app->cursorJustEnteredWindow = true;
+    }
+  }
+
+  static void WindowResizeCallback(GLFWwindow* window, int newWidth, int newHeight)
+  {
+    Application* app = static_cast<Application*>(glfwGetWindowUserPointer(window));
+    app->windowWidth = static_cast<uint32_t>(newWidth);
+    app->windowHeight = static_cast<uint32_t>(newHeight);
+    app->OnWindowResize(app->windowWidth, app->windowHeight);
+  }
+};
+
+std::string Application::LoadFile(std::string_view path)
+{
+  std::ifstream file{path.data()};
+  return {std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()};
+}
+
+Application::Application(const CreateInfo& createInfo)
+{
+  // Initialiize GLFW
+  if (!glfwInit())
+  {
+    throw std::runtime_error("Failed to initialize GLFW");
+  }
+
+  glfwSetErrorCallback([](int, const char* desc) { std::cout << "GLFW error: " << desc << '\n'; });
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_MAXIMIZED, createInfo.maximize);
+  glfwWindowHint(GLFW_DECORATED, createInfo.decorate);
+  glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE);
+  glfwWindowHint(GLFW_SRGB_CAPABLE, GLFW_TRUE);
+  glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+
+  //GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+  //const GLFWvidmode* videoMode = glfwGetVideoMode(monitor);
+  //window = glfwCreateWindow(videoMode->width, videoMode->height, createInfo.name.data(), monitor, nullptr);
+  window = glfwCreateWindow(800, 600, createInfo.name.data(), nullptr, nullptr);
+
+  int xSize{};
+  int ySize{};
+  glfwGetFramebufferSize(window, &xSize, &ySize);
+  windowWidth = static_cast<uint32_t>(xSize);
+  windowHeight = static_cast<uint32_t>(ySize);
+
+  if (!window)
+  {
+    throw std::runtime_error("Failed to create window");
+  }
+
+  glfwSetWindowUserPointer(window, this);
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(createInfo.vsync ? 1 : 0);
+
+  glfwSetCursorPosCallback(window, ApplicationAccess::CursorPosCallback);
+  glfwSetCursorEnterCallback(window, ApplicationAccess::CursorEnterCallback);
+  glfwSetFramebufferSizeCallback(window, ApplicationAccess::WindowResizeCallback);
+
+  // Initialize OpenGL.
+  int version = gladLoadGL(glfwGetProcAddress);
+  if (version == 0)
+  {
+    glfwTerminate();
+    throw std::runtime_error("Failed to initialize OpenGL");
+  }
+
+  // Set up the GL debug message callback.
+  glEnable(GL_DEBUG_OUTPUT);
+  glDebugMessageCallback(OpenglErrorCallback, nullptr);
+  glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+  glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+
+  // Set useful GL state.
+  glEnable(GL_FRAMEBUFFER_SRGB);
+
+  // Initialize ImGui and a backend for it.
+  // Because we allow the GLFW backend to install callbacks, it will automatically call our own that we provided.
+  ImGui::CreateContext();
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init();
+  ImGui::StyleColorsDark();
+}
+
+Application::~Application()
+{
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
+  glfwTerminate();
+}
+
+void Application::Run()
+{
+  // The main loop.
+  double prevFrame = glfwGetTime();
+  while (!glfwWindowShouldClose(window))
+  {
+    double curFrame = glfwGetTime();
+    double dt = curFrame - prevFrame;
+    prevFrame = curFrame;
+
+    cursorFrameOffset = {0.0, 0.0};
+    glfwPollEvents();
+
+    // Close the app if the user presses Escape.
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE))
+    {
+      glfwSetWindowShouldClose(window, true);
+    }
+
+    // Start a new ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    // Update the main mainCamera.
+    // WASD can be used to move the camera forwards, backwards, and side-to-side.
+    // The mouse can be used to orient the camera.
+    // Not all examples will use the main camera.
+    const float dtf = static_cast<float>(dt);
+    const glm::vec3 forward = mainCamera.GetForwardDir();
+    const glm::vec3 right = glm::normalize(glm::cross(forward, {0, 1, 0}));
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+      mainCamera.position += forward * dtf * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+      mainCamera.position -= forward * dtf * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+      mainCamera.position += right * dtf * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+      mainCamera.position -= right * dtf * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+      mainCamera.position.y -= dtf * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+      mainCamera.position.y += dtf * cameraSpeed;
+    mainCamera.yaw += static_cast<float>(cursorFrameOffset.x * cursorSensitivity);
+    mainCamera.pitch += static_cast<float>(cursorFrameOffset.y * cursorSensitivity);
+    mainCamera.pitch = glm::clamp(mainCamera.pitch, -glm::half_pi<float>() + 1e-4f, glm::half_pi<float>() - 1e-4f);
+
+    // Call the application's overriden functions each frame.
+    OnUpdate(dt);
+    OnRender(dt);
+    OnGui();
+
+    // Updates ImGui.
+    // A frame marker is inserted to distinguish ImGui rendering from the application's in a debugger.
+    {
+      ImGui::Render();
+      auto marker = Fwog::ScopedDebugMarker("Draw GUI");
+      ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+      ImGui::EndFrame();
+    }
+
+    glfwSwapBuffers(window);
+  }
+}
