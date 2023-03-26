a6dc60828075dd85cef88262fd90836eaa308a9a
Jake Ryan <jakekryan101@gmail.com>
2023-03-26 04:11:24 -0700

    Initial docs

---
+++ docs/conf.py
@@ -0,0 +1,55 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+
+import subprocess, os
+
+def configureDoxyfile(input_dir, output_dir):
+    with open('Doxyfile.in', 'r') as file :
+        filedata = file.read()
+
+    filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
+    filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
+
+    with open('Doxyfile', 'w') as file:
+        file.write(filedata)
+
+# Check if we're running on Read the Docs' servers
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Fwog'
+copyright = '2023, Jake Ryan'
+author = 'Jake Ryan'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['breathe']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# Breathe Configuration
+breathe_default_project = "Fwog"
+
+breathe_projects = {}
+
+if read_the_docs_build:
+    input_dir = '../include/Fwog'
+    output_dir = 'build'
+    configureDoxyfile(input_dir, output_dir)
+    subprocess.call('doxygen', shell=True)
+    breathe_projects['Fwog'] = output_dir + '/xml'
---
+++ docs/index.rst
@@ -0,0 +1,36 @@
+.. Fwog documentation master file, created by
+   sphinx-quickstart on Sat Mar 25 03:12:07 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Fwog's documentation!
+================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Summary
+=======
+Fwog is a low-level OpenGL 4.6 abstraction written in C++20 which aims to offer improve ergonomics and eliminate deprecated functionality.
+
+Getting Started
+===============
+::
+
+    git clone https://github.com/JuanDiegoMontoya/Fwog.git
+    cd Fwog
+    mkdir build
+    cd build
+    cmake ..
+
+Test doc pls ignore
+===================
+.. doxygenfile:: Pipeline.h
+
+Indices and tables
+==================
+
+* :ref:`genindex`
