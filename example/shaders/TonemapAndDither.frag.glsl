f9775b70638a99cc65a398a10f44a45ad7a7224f
Jake Ryan <jakekryan101@gmail.com>
2023-06-03 02:43:58 -0700

    Use better tone mapper for gltf viewer sample

--- example/shaders/TonemapAndDither.frag.glsl
+++ example/shaders/TonemapAndDither.frag.glsl
@@ -6,6 +6,95 @@ layout(binding = 1) uniform sampler2D s_noise;
 layout(location = 0) in vec2 v_uv;
 layout(location = 0) out vec4 o_finalColor;
 
+// AgX implementation from here: https://www.shadertoy.com/view/Dt3XDr
+vec3 xyYToXYZ(vec3 xyY)
+{
+    float Y = xyY.z;
+    float X = (xyY.x * Y) / xyY.y;
+    float Z = ((1.0f - xyY.x - xyY.y) * Y) / xyY.y;
+
+    return vec3(X, Y, Z);
+}
+
+vec3 Unproject(vec2 xy)
+{
+    return xyYToXYZ(vec3(xy.x, xy.y, 1));				
+}
+
+mat3 PrimariesToMatrix(vec2 xy_red, vec2 xy_green, vec2 xy_blue, vec2 xy_white)
+{
+    vec3 XYZ_red = Unproject(xy_red);
+    vec3 XYZ_green = Unproject(xy_green);
+    vec3 XYZ_blue = Unproject(xy_blue);
+    vec3 XYZ_white = Unproject(xy_white);
+
+    mat3 temp = mat3(XYZ_red.x,	  1.0f, XYZ_red.z,
+                     XYZ_green.x, 1.0f, XYZ_green.z,
+                     XYZ_blue.x,  1.0f, XYZ_blue.z);
+    vec3 scale = inverse(temp) * XYZ_white;
+
+    return mat3(XYZ_red * scale.x, XYZ_green * scale.y, XYZ_blue * scale.z);
+}
+
+mat3 ComputeCompressionMatrix(vec2 xyR, vec2 xyG, vec2 xyB, vec2 xyW, float compression)
+{
+    float scale_factor = 1.0f / (1.0f - compression);
+    vec2 R = mix(xyW, xyR, scale_factor);
+    vec2 G = mix(xyW, xyG, scale_factor);
+    vec2 B = mix(xyW, xyB, scale_factor);
+    vec2 W = xyW;
+
+    return PrimariesToMatrix(R, G, B, W);
+}
+
+float DualSection(float x, float linear, float peak)
+{
+	// Length of linear section
+	float S = (peak * linear);
+	if (x < S) {
+		return x;
+	} else {
+		float C = peak / (peak - S);
+		return peak - (peak - S) * exp((-C * (x - S)) / peak);
+	}
+}
+
+vec3 DualSection(vec3 x, float linear, float peak)
+{
+	x.x = DualSection(x.x, linear, peak);
+	x.y = DualSection(x.y, linear, peak);
+	x.z = DualSection(x.z, linear, peak);
+	return x;
+}
+
+vec3 AgX_DS(vec3 color_srgb, float exposure, float saturation, float linear, float peak, float compression)
+{
+  vec3 workingColor = max(color_srgb, 0.0f) * pow(2.0, exposure);
+
+  mat3 sRGB_to_XYZ = PrimariesToMatrix(vec2(0.64,0.33),
+                                       vec2(0.3,0.6), 
+                                       vec2(0.15,0.06), 
+                                       vec2(0.3127, 0.3290));
+  mat3 adjusted_to_XYZ = ComputeCompressionMatrix(vec2(0.64,0.33),
+                                                  vec2(0.3,0.6), 
+                                                  vec2(0.15,0.06), 
+                                                  vec2(0.3127, 0.3290), compression);
+  mat3 XYZ_to_adjusted = inverse(adjusted_to_XYZ);
+  mat3 sRGB_to_adjusted = sRGB_to_XYZ * XYZ_to_adjusted;
+
+  workingColor = sRGB_to_adjusted * workingColor;
+  workingColor = clamp(DualSection(workingColor, linear, peak), 0.0, 1.0);
+  
+  vec3 luminanceWeight = vec3(0.2126729,  0.7151522,  0.0721750);
+  vec3 desaturation = vec3(dot(workingColor, luminanceWeight));
+  workingColor = mix(desaturation, workingColor, saturation);
+  workingColor = clamp(workingColor, 0.f, 1.f);
+
+  workingColor = inverse(sRGB_to_adjusted) * workingColor;
+
+  return workingColor;
+}
+
 // ACES fitted
 // from https://github.com/TheRealMJP/BakingLab/blob/master/BakingLab/ACES.hlsl
 
@@ -44,7 +133,8 @@ vec3 ACESFitted(vec3 color)
     return color;
 }
 
-vec3 linear_to_srgb(vec3 linearColor)
+// sRGB OETF
+vec3 linear_to_nonlinear_srgb(vec3 linearColor)
 {
   bvec3 cutoff = lessThan(linearColor, vec3(0.0031308));
   vec3 higher = vec3(1.055) * pow(linearColor, vec3(1.0 / 2.4)) - vec3(0.055);
@@ -63,8 +153,9 @@ vec3 apply_dither(vec3 color, vec2 uv)
 void main()
 {
   vec3 hdrColor = textureLod(s_sceneColor, v_uv, 0).rgb;
-  vec3 ldrColor = ACESFitted(hdrColor);
-  vec3 srgbColor = linear_to_srgb(ldrColor);
+  //vec3 ldrColor = ACESFitted(hdrColor);
+  vec3 ldrColor = AgX_DS(hdrColor, -3, 1.0, 0.18, 1, 0.15);
+  vec3 srgbColor = linear_to_nonlinear_srgb(ldrColor);
   vec3 ditheredColor = apply_dither(srgbColor, v_uv);
 
   o_finalColor = vec4(ditheredColor, 1.0);
