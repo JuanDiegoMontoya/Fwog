15ddb839d50b37adae95600544bede5bea633598
deccer <decccer@outlook.com>
2023-08-25 12:24:32 +0200

    Add error handling when monitor is null

--- example/common/Application.cpp
+++ example/common/Application.cpp
@@ -175,6 +175,10 @@ Application::Application(const CreateInfo& createInfo)
   glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
 
   GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+  if (monitor == nullptr)
+  {
+    throw std::runtime_error("No Monitor detected");
+  }
   const GLFWvidmode* videoMode = glfwGetVideoMode(monitor);
   window = glfwCreateWindow(int(videoMode->width * .75), int(videoMode->height * .75), createInfo.name.data(), nullptr, nullptr);
   if (!window)
