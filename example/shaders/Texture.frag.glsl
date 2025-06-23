b228b0e3ad324f9735e5c179b27eb525e44a597c
Jake Ryan <38368197+JuanDiegoMontoya@users.noreply.github.com>
2025-06-22 17:51:43 -0700

    Fixed missing binding specifier

--- example/shaders/Texture.frag.glsl
+++ example/shaders/Texture.frag.glsl
@@ -2,7 +2,7 @@
 
 layout(location = 0) in vec2 v_uv;
 
-layout(location = 0) uniform sampler2D s_texture;
+layout(binding = 0) uniform sampler2D s_texture;
 
 layout(location = 0) out vec4 o_color;
 
